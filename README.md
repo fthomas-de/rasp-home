9cf6e2d647fb37e333ce024cb2970d481f151a2d
Florian Thomas <flothomas@web.de>
2014-05-29 01:43:35 +0200

    add buttons

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 private project using my raspberry pi
 
-app: controlling some wlan plug 
+app: controlling some wlan plugs
 
 - 433Mhz RF Wireless Transmitter-Receiver-Modul
 - düwi plug sockets
--- app/gen/com/example/rasp_home/R.java
+++ app/gen/com/example/rasp_home/R.java
@@ -23,13 +23,16 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int pfeil=0x7f020001;
+        public static final int stecker=0x7f020002;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f070000;
-        public static final int button1=0x7f070003;
         public static final int connect=0x7f070002;
+        public static final int imageButton1=0x7f070004;
+        public static final int imageButton2=0x7f070005;
         public static final int ipaddress=0x7f070001;
-        public static final int set=0x7f070004;
+        public static final int set=0x7f070003;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
