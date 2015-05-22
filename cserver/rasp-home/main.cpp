5ce16f7bacb424320a2c68c05faa785d45b6e888
Florian Thomas <fthomas@tzi.de>
2015-05-22 11:26:24 +0200

    push

--- cserver/rasp-home/main.cpp
+++ cserver/rasp-home/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <string>
-#include <stdio.h>
 #include "RCSwitch.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-using namespace std;
+//using namespace std;
 
 int main(int argc, char *argv[]) {
-    int PIN = 0; // equals Pin 11 or GPIO 17
+    int PIN = 0;
     char* systemCode = argv[1];
     int unitCode = atoi(argv[2]);
     int command  = atoi(argv[3]);
@@ -14,19 +14,5 @@ int main(int argc, char *argv[]) {
     if (wiringPiSetup () == -1) return 1;
     printf("sending systemCode[%s] unitCode[%i] command[%i]\n", systemCode, unitCode, command);
     RCSwitch mySwitch = RCSwitch();
-    mySwitch.enableTransmit(PIN);
 
-    switch(command) {
-        case 1:
-            mySwitch.switchOn(systemCode, unitCode);
-            break;
-        case 0:
-            mySwitch.switchOff(systemCode, unitCode);
-            break;
-        default:
-            printf("command[%i] is unsupported\n", command);
-            return -1;
-    }
-
-    return 0;
 }
